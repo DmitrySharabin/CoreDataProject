f74896d5c88fc5a55bcd729e00f3ba805961021f
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-12-09 14:43:52 +0300

    Day 59 — Modify the predicate string parameter to be an enum

--- CoreDataProject/FilteredList.swift
+++ CoreDataProject/FilteredList.swift
@@ -23,6 +23,14 @@ import SwiftUI
 //}
 
 struct FilteredList<T: NSManagedObject, Content: View>: View {
+    enum Predicate: String {
+        case beginsWith = "BEGINSWITH"
+        case contains = "CONTAINS"
+        case eq = "=="
+        case lt = "<"
+        case gt = ">"
+    }
+    
     @FetchRequest var fetchRequest: FetchedResults<T>
     
     let content: (T) -> Content
@@ -33,8 +41,8 @@ struct FilteredList<T: NSManagedObject, Content: View>: View {
         }
     }
     
-    init(filterKey: String, filterValue: String, predicate: String, @ViewBuilder content: @escaping (T) -> Content) {
-        _fetchRequest = FetchRequest<T>(sortDescriptors: [], predicate: NSPredicate(format: "%K \(predicate) %@", filterKey, filterValue))
+    init(filterKey: String, filterValue: String, predicate: Predicate, @ViewBuilder content: @escaping (T) -> Content) {
+        _fetchRequest = FetchRequest<T>(sortDescriptors: [], predicate: NSPredicate(format: "%K \(predicate.rawValue) %@", filterKey, filterValue))
         self.content = content
     }
 }
