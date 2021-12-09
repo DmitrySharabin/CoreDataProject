33fc4d05d9624b2266f579adac92e31f4a91e97d
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-12-09 15:00:06 +0300

    Day 59 — Remove unnecessary code

--- CoreDataProject/FilteredList.swift
+++ CoreDataProject/FilteredList.swift
@@ -8,20 +8,6 @@
 import CoreData
 import SwiftUI
 
-//struct FilteredList: View {
-//    @FetchRequest var fetchRequest: FetchedResults<Singer>
-//
-//    var body: some View {
-//        List(fetchRequest, id: \.self) { singer in
-//            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
-//        }
-//    }
-//
-//    init(filter: String) {
-//        _fetchRequest = FetchRequest<Singer>(sortDescriptors: [], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter))
-//    }
-//}
-
 struct FilteredList<T: NSManagedObject, Content: View>: View {
     enum Predicate: String {
         case beginsWith = "BEGINSWITH"
